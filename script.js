2c2fef692a96c8c932c464414fe69685893471b1
Whatakel <kelvinmlcardoso@gmail.com>
2024-07-17 14:03:16 -0300

    finalização ctn pedidos fixed

--- script.js
+++ script.js
@@ -18,10 +18,34 @@ window.addEventListener('resize', () => {
 })
 modalPedidos();
 
-var ctnPedidos = document.querySelector(".fixed-column");
-var topWindowHeight = window.scrollY;
-var teste = window.innerHeight
+const ctnPedidos = document.querySelector(".fixed-column");
+const filtroIcone = document.querySelector(".icone-filtro");
+var ctnFiltroHeight;
+var windowScrollY;
 
-window.addEventListener('scroll', () => {
-    console.log(teste)
-})
+function pedidosFixed(){
+    var windowScrollY = window.scrollY;
+    var windowHight = window.innerHeight;
+    var ctnFiltroHeight = document.querySelector(".gestao-filtros").getBoundingClientRect().height;
+    var filtroExpand = document.querySelector(".icone-filtro").getAttribute("aria-expanded");
+    
+    if(filtroExpand === "false"){
+        ctnPedidos.style.top = windowScrollY + 'px';
+        ctnPedidos.style.height = "auto"
+
+    }if(windowScrollY > ctnFiltroHeight && filtroExpand === "true"){
+        ctnPedidos.style.top = windowScrollY - ctnFiltroHeight + 'px';
+
+    }if(windowScrollY < ctnFiltroHeight){
+        ctnPedidos.style.top = "inherit"
+
+    }if(filtroExpand === "true"){
+        setTimeout(() =>{
+            var ctnPosition = document.querySelector(".fixed-column").getBoundingClientRect().top
+            ctnPedidos.style.height = `calc(${windowHight - ctnPosition}px - 1rem)`
+            console.log(ctnPosition)
+            }, 300);     
+        }
+    }
+filtroIcone.addEventListener('click', pedidosFixed);
+window.addEventListener('scroll', pedidosFixed);
